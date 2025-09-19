fa3a14757e9589f24c4cd93c180a3f54bc20d6fe
hoopdev <taga.kotaro.62d@st.kyoto-u.ac.jp>
2025-09-19 09:25:34 +0900

    fix: neovim configured with lazy

--- home/common/cli/neovim.nix
+++ home/common/cli/neovim.nix
@@ -1,52 +1,31 @@
-{ pkgs, inputs, ... }:
-let
-  # Build dpp and related plugins from flake inputs
-  dppPlugins = {
-    dpp-vim = pkgs.vimUtils.buildVimPlugin {
-      name = "dpp.vim";
-      src = inputs.dpp-vim;
-    };
-    denops-vim = pkgs.vimUtils.buildVimPlugin {
-      name = "denops.vim";
-      src = inputs.denops-vim;
-    };
-    dpp-ext-installer = pkgs.vimUtils.buildVimPlugin {
-      name = "dpp-ext-installer";
-      src = inputs.dpp-ext-installer;
-    };
-    dpp-ext-lazy = pkgs.vimUtils.buildVimPlugin {
-      name = "dpp-ext-lazy";
-      src = inputs.dpp-ext-lazy;
-    };
-    dpp-ext-toml = pkgs.vimUtils.buildVimPlugin {
-      name = "dpp-ext-toml";
-      src = inputs.dpp-ext-toml;
-    };
-    dpp-protocol-git = pkgs.vimUtils.buildVimPlugin {
-      name = "dpp-protocol-git";
-      src = inputs.dpp-protocol-git;
-    };
-  };
-in
+{ pkgs, ... }:
 {
   programs.nixvim = {
-    # This just enables NixVim.
     enable = true;
 
-    # Load external Lua configuration
-    extraConfigLua = builtins.readFile ./nvim.lua;
+    # Load external Lua configuration (init.lua)
+    extraConfigLua = builtins.readFile ./init.lua;
 
-    # Use extraPlugins:
-    extraPlugins = with pkgs.vimPlugins; [ 
-      vim-toml 
-    ] ++ (with dppPlugins; [
-      # dpp plugin manager and extensions
-      dpp-vim
-      denops-vim
-      dpp-ext-installer
-      dpp-ext-lazy
-      dpp-ext-toml
-      dpp-protocol-git
-    ]);
+    # Add lazy.nvim plugin to be available for bootstrapping
+    extraPlugins = with pkgs.vimPlugins; [
+      lazy-nvim
+    ];
+
+    # Enable lazy loading support in nixvim
+    plugins = {
+      # Disable nixvim's built-in package management to let lazy.nvim handle it
+      lazy = {
+        enable = true;
+      };
+    };
+
+    # Performance settings
+    performance = {
+      byteCompileLua = {
+        enable = true;
+        nvimRuntime = true;
+        plugins = true;
+      };
+    };
   };
 }
